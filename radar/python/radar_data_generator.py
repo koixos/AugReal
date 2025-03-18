82b03b6d1e84f6ee45ea9af40c03e8034af169d6
koixos <koioszey@gmail.com>
2025-03-18 15:46:41 +0300

    test

--- radar/python/radar_data_generator.py
+++ radar/python/radar_data_generator.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import numpy as np
+import kagglehub
 
-num_points = 2000
+kagglehub.dataset_download("ahtshamzafar/tiawr6843aop-gesture-language-data", path="./sample-dataset")
+
+print("Path to dataset files:")
+
+'''num_points = 2000
 x = np.random.uniform(-10, 10, num_points)
 y = np.random.uniform(-10, 10, num_points)
 z = np.random.uniform(-10, 10, num_points)
 
 df = pd.DataFrame({'x':x, 'y':y, 'z':z})
-df.to_csv("radar_data.csv", index=False)
+df.to_csv("radar_data.csv", index=False)'''
